aa556aea78e736dab07f3c17ac564feb2580954b
jscoba <jscoba@gmail.com>
2013-06-06 21:40:12 +0200

    Update procesar.php

--- servidor_preguntas/procesar.php
+++ servidor_preguntas/procesar.php
@@ -7,4 +7,7 @@ $v3=$_POST['ansB'];
 $v4=$_POST['ansC'];
 $v5=$_POST['ansD'];
 $v6=$_POST['anstrue'];
+
+$query="INSERT questions '(pregunta,A,B,C,D,correcta) VALUES ('$v1','$v2','$v3','$v4','$v5','$v6')";
+$result = mysql_query($query) or die(mysql_error());
 ?>
