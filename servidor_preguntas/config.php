493708cfa53f0f0b6b9afa10269a945512b08c2a
jscoba <jscoba@gmail.com>
2013-06-06 15:25:30 +0300

    Create config.php

---
+++ servidor_preguntas/config.php
@@ -0,0 +1,10 @@
+<?php
+
+  define('MYSQL_SERVER',          'localhost');
+  define('MYSQL_DATABASE',        'nosy_cube');
+  define('MYSQL_USERNAME',        'root');
+  define('MYSQL_PASSWORD',        'toor');
+  define('PRIVATE_LOCATION'),			'/var/www/jaup/private/');
+
+
+?>
