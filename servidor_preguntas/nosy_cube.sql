5eaed5fee870c474aa8f0c7c4b08696591c07dea
Javier Sáez de la Coba <jscoba@gmail.com>
2013-06-06 20:03:00 +0200

    Base de datos creada

---
+++ servidor_preguntas/nosy_cube.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb7
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 06-06-2013 a las 20:01:14
+-- Versión del servidor: 5.1.66
+-- Versión de PHP: 5.3.3-7+squeeze15
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `nosy_cube`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `ID` int(255) NOT NULL AUTO_INCREMENT,
+  `pregunta` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `A` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `B` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `C` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `D` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `correcta` text CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `questions`
+--
+
