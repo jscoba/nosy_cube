67df25cc3bfc7fda700d83632d21716f9bca342a
Javier Sáez de la Coba <jscoba@gmail.com>
2013-06-13 23:27:12 +0200

    First stable commit

--- servidor_preguntas/generador.php
+++ servidor_preguntas/generador.php
@@ -6,14 +6,6 @@ include_once "conectar.php";
   $query = "SELECT * FROM `questions`";
   $result = mysql_query($query);
   $numero = 0;
-//while($row = mysql_fetch_array($result))
-//{
-//$salida='		Case '.$row["ID"].'
-//						GoTo q'.$row["ID"].'
-//';
-//	print nl2br($salida);
-	
-//}
 
 while($row = mysql_fetch_array($result))
 {
@@ -37,26 +29,26 @@ while($row = mysql_fetch_array($result))
 			while ($temporal){			
 			if (strlen($quest1) <= 16 AND strlen($quest1) + strlen($temporal) <= 16 AND $quest1b==0)
 			{
-				$quest1.= " " . $temporal;
+				$quest1.=  $temporal." " ;
 				$temporal =  strtok (" ");
 			}else{
 				if (strlen($quest2) <= 16 AND strlen($quest2) + strlen($temporal) <= 16 AND $quest2b==0)
 				{
 					$quest1b = 1;					
-					$quest2.= " " . $temporal;
+					$quest2.= $temporal." " ;
 					$temporal =  strtok (" ");					
 				}else{
 					if (strlen($quest3) <= 16 AND strlen($quest3) + strlen($temporal) <= 16 AND $quest3b==0)
 					{
 						$quest2b = 1;
-						$quest3.= " " . $temporal;
+						$quest3.= $temporal." " ;
 						$temporal =  strtok (" ");				
 					}else{
-						if (strlen($quest4) <= 20 //AND strlen($quest4) + strlen($temporal) <= 20
+						if (strlen($quest4) <= 16 //AND strlen($quest4) + strlen($temporal) <= 20
 						)
 						{
 							$quest3b = 1;
-							$quest4.= " " . $temporal;
+							$quest4.= $temporal." " ;
 							$temporal =  strtok (" ");		
 						}
 					}
